29d998832aa1e28158ff35fd28c096937f10bc13
Aivin V. Solatorio <avsolatorio@gmail.com>
2024-04-02 00:01:39 -0400

    Add docstring to utils and update deps
    Signed-off-by: Aivin V. Solatorio <avsolatorio@gmail.com>

--- src/llm_lang/utils.py
+++ src/llm_lang/utils.py
@@ -1,9 +1,21 @@
 import pandas as pd
 import tiktoken
 from .external import flores
+import datasets
 
 
-def get_dataset(lang, lang2=None, split="dev"):
+def get_dataset(lang: str, lang2: str = None, split: str = "dev"):
+    """Get the dataset for the given language(s) and split.
+
+    Args:
+        lang (str): The language code.
+        lang2 (str): The second language code.
+        split (str): The split to use.
+
+    Returns:
+        Dataset: The dataset.
+    """
+
     config_name = lang
     if lang2:
         config_name = f"{lang}-{lang2}"
@@ -14,11 +26,24 @@ def get_dataset(lang, lang2=None, split="dev"):
     return fl.as_dataset(split=split)
 
 
-def get_data_column(ds, lang):
+def get_data_column(ds: datasets.arrow_dataset.Dataset, lang: str):
+    """Get the data column for the given language.
+
+    Args:
+        ds (Dataset): The dataset.
+        lang (str): The language code.
+
+    Returns:
+        List[str]: The data column.
+    """
+
+    if lang == "all":
+        return [k for l in ds.column_names if l.startswith("sentence_") for k in ds[l]]
+
     return ds[f"sentence_{lang}"]
 
 
-def get_tokens(ds, lang, tokenizer=None, model_name=None):
+def get_tokens(ds: datasets.arrow_dataset.Dataset, lang: str, tokenizer=None, model_name: str = None):
     if tokenizer:
         assert model_name is None, "Either tokenizer or model_name must be provided"
     else:
@@ -29,7 +54,18 @@ def get_tokens(ds, lang, tokenizer=None, model_name=None):
     return tokenizer.encode_batch(get_data_column(ds, lang))
 
 
-def get_token_stats(ds, lang: str, model_name: str = None):
+def get_token_stats(ds: datasets.arrow_dataset.Dataset, lang: str, model_name: str = None):
+    """Get token statistics for the given language.
+
+    Args:
+        ds (Dataset): The dataset.
+        lang (str): The language code.
+        model_name (str): The model name.
+
+    Returns:
+        Dict: The token statistics.
+    """
+
     tokens_list = get_tokens(ds, lang, model_name=model_name)
     n_tokens = [len(t) for t in tokens_list]
     token_stats = pd.Series(n_tokens).describe()
